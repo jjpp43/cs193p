bf34de525f166f38426065fe69bd272590485989
Jazzy <jparkdev63@gmail.com>
2025-12-19 15:08:50 -0500

    Initial commit

---
+++ cs193p/cs193p/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cs193p
+//
+//  Created by Junna on 12/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cs193p/cs193p/cs193pApp.swift
@@ -0,0 +1,17 @@
+//
+//  cs193pApp.swift
+//  cs193p
+//
+//  Created by Junna on 12/19/25.
+//
+
+import SwiftUI
+
+@main
+struct cs193pApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
